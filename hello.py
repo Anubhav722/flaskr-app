8bda17501f1416fe371b9882ee59137283522798
Anubhav722 <anubhavs286@gmail.com>
2018-01-14 18:48:16 +0530

    add var rules

--- hello.py
+++ hello.py
@@ -2,12 +2,25 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/hello/')
+@app.route('/hello')
 def hell():
     return "check"
 
+# building dynamic urls
+@app.route('/hello/<name>')
+def hello_name(name):
+    return 'hello {}'.format(name)
+
 # another way to add urls is to write 
 # app.add_url_rule('hello', hell)
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+   return 'Revision Number %f' % revNo
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
 
 @app.route('/')
 def hello_world():
